dd7a20ad6f5750b9a0386a0d1c76fb8e56db08b1
Ethan <opfire990@gmail.com>
2023-12-06 14:34:34 -0800

    Initial File Creation

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+#include <iostream>
+
+using std::cout, std::vector;
+std::vector<int> bubble_sort(const std::vector<int>& dataset);
+
+int main() {
+    return 0;
+}
---
+++ src/sorts.h
@@ -0,0 +1,6 @@
+#include <vector>
+
+std::vector<int> bubble_sort(const std::vector<int>& dataset);
+std::vector<int> insertion_sort(const std::vector<int>& dataset);
+std::vector<int> selection_sort(const std::vector<int>& dataset);
+std::vector<int> merge_sort(const std::vector<int>& dataset);
